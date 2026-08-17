hanzoai/bot

perf(state): cap the per-agent SQLite handle cache with LRU eviction (#111411)
* perf(state): cap the per-agent SQLite handle cache with LRU eviction

Multi-tenant hosts open one WAL database per agent (~3 file descriptors
each); the process-local handle cache was unbounded, so large fleets
exhausted descriptors. Cache hits now refresh LRU recency, and cache-miss
opens evict the oldest non-transactional handle before constructing the
new one, capped at 64 open handles. Eviction closes the process-local
handle only; registry rows and durable data are untouched and evicted
databases reopen transparently on next access.

* fix(state): fleet-wide introspection reads stop opening writable agent databases

Live testing an 81-agent gateway surfaced three full-fleet sweeps that
opened every agent's SQLite database writable (schema ensure + registry
write transaction per open): the 60s health snapshot's per-agent session
summaries, usage-cost cache reads behind the gateway usage endpoints, and
the zalouser doctor detector that ran from every CLI startup. Each sweep
churned the new bounded handle cache and saturated the event loop.

Session listing gains a readonly non-registering variant (SDK exposes it
as an additive readOnly flag on listSessionEntries); health and usage
reads use it, with transient-lock tolerance owned by the health caller.
The zalouser detector gates on the channel/credentials actually existing,
collects once per pass, and detection reads go readonly while migration
apply stays writable. The usage refresh queue's flat 50ms busy retry
becomes exponential backoff capped at 5s, and refreshing summaries retain
their cache timestamp so the 30s TTL gates fleet rescans.

* perf(state): make per-agent handle eviction and reopen cheap

Eviction only works if reopen is cheap. Reopens after eviction previously
repeated full first-open work; ensureOpenClawAgentSchema takes BEGIN
IMMEDIATE and the registry upsert writes the shared state DB, so reopens
blocked in synch

src/state/openclaw-agent-db.ts
@@ -70,14 +70,22 @@ export { resolveOpenClawAgentSqlitePath } from "./openclaw-agent-db.paths.js";
  * OpenClaw state database for discovery and maintenance.
  */
 const OPENCLAW_AGENT_DB_SLOW_OPEN_MS = 1_000;
+// Each WAL database consumes roughly three file descriptors, so the fixed cap
+// satisfies the bounded-cache policy within a predictable FD budget, without config.
+export const OPENCLAW_AGENT_DB_OPEN_HANDLE_CAP = 64;
 const agentDbLog = createSubsystemLogger("state/agent-db");
 const cachedDatabases = new Map<string, OpenClawAgentDatabase>();
+// External schema changes under a live process are unsupported: doctor migrations
+// require restart, so successful owner/schema validation is process-stable.
+const validatedAgentDatabasePaths = new Map<string, string>();
 const terminalOpenLatch = createSqliteTerminalOpenLatch({
   closeByPath: closeOpenClawAgentDatabaseByPath,
 });
 
 /** Latch background verification damage so later opens fail without rescanning. */
 export function recordOpenClawAgentDatabaseOpenFailure(pathname: string, error: Error): void {
+  // Quarantine revokes this process's trust because doctor may replace the file.
+  validatedAgentDatabasePaths.delete(path.resolve(pathname));
   terminalOpenLatch.record(pathname, error);
 }
 
@@ -153,6 +161,8 @@ export function openOpenClawAgentDatabase(
         `OpenClaw agent database ${pathname} is already open for agent ${cached.agentId}; requested agent ${agentId}.`,
       );
     }
+    cachedDatabases.delete(pathname);
+    cachedDatabases.set(pathname, cached);
     return cached;
   }
   // Latched paths are quarantined; every fresh open fails fast here until
@@ -187,14 +197,24 @@ export function openOpenClawAgentDatabase(
   }
   const openStartedAt = Date.now();
   ensureOpenClawAgentDatabasePermissions(pathname, databaseOptions);
+  // Free a slot before constructing the new handle: under real descriptor
+  // pressure the 65th open would otherwise fail before eviction could run.
+  evictLruAgentDatabaseHandles();
   const sqlite = requireNodeSqlite();
   const db = new sqlite.DatabaseSync(pathname);
+  // Eviction churn must avoid schema/registry busy waits on the event loop while
+  // reconcile workers hold write transactions on these same agent databases.
+  const isValidatedReopen = validatedAgentDatabasePaths.get(pathname) === agentId;
   const walMaintenance = (() => {
     let maintenance: OpenClawAgentDatabase["walMaintenance"] | undefined;
     try {
       db.exec(`PRAGMA busy_timeout = ${OPENCLAW_SQLITE_BUSY_TIMEOUT_MS};`);
-      assertSupportedAgentSchemaVersion(db, pathname);
-      assertExistingAgentSchemaOwner(readExistingAgentSchemaMeta(db), agentId, pathname);
+      if (!isValidatedReopen) {
+        assertSupportedAgentSchemaVersion(db, pathname);
+        assertExistingAgentSchemaOwner(readExistingAgentSchemaMeta(db), agentId, pathname);
+      }
+      // Integrity is not process-stable: the file can be damaged while evicted.
+      // This guard is read-only (no busy waits), so every physical open pays it.
       assertAgentDatabaseIntegrityBeforeMutation(db, pathname);
       configureSqlitePreSchemaPragmas(db, {
         busyTimeoutMs: OPENCLAW_SQLITE_BUSY_TIMEOUT_MS,
@@ -206,7 +226,9 @@ export function openOpenClawAgentDatabase(
         foreignKeys: true,
         synchronous: "NORMAL",
       });
-      ensureOpenClawAgentSchema(db, agentId, pathname);
+      if (!isValidatedReopen) {
+        ensureOpenClawAgentSchema(db, agentId, pathname);
+      }
       return maintenance;
     } catch (err) {
       maintenance?.close();
@@ -222,11 +244,14 @@ export function openOpenClawAgentDatabase(
   })();
   ensureOpenClawAgentDatabasePermissions(pathname, databaseOptions);
   const database = { agentId, db, path: pathname, walMaintenance };
-  try {
-    registerOpenClawAgentDatabase({ agentId, path: pathname, env: options.env });
-  } catch (error) {
-    closeCachedOpenClawAgentDatabase(database);
-    throw error;
+  if (!isValidatedReopen) {
+    try {
+      registerOpenClawAgentDatabase({ agentId, path: pathname, env: options.env });
+    } catch (error) {
+      closeCachedOpenClawAgentDatabase(database);
+      throw error;
+    }
+    validatedAgentDatabasePaths.set(pathname, agentId);
   }
   cachedDatabases.set(pathname, database);
   terminalOpenLatch.clear(pathname);
@@ -312,11 +337,56 @@ export function runOpenClawAgentWriteTransaction<T>(
 
 let unregisterExitClose: (() => void) | null = null;
 
-function closeCachedOpenClawAgentDatabase(database: OpenClawAgentDatabase): void {
-  database.walMaintenance.close();
+function closeCachedOpenClawAgentDatabase(
+  database: OpenClawAgentDatabase,
+  options: { eviction?: boolean } = {},
+): void {
+  // Eviction must stay cheap: PASSIVE skips waiting on concurrent readers,
+  // whose drained TRUNCATE checkpoints blocked the event loop for seconds.
+  database.walMaintenance.close(options.eviction ? { checkpointMode: "PASSIVE" } : undefined);
   clearNodeSqliteKyselyCacheForDatabase(database.db);
   if (database.db.isOpen) {
     database.db.close();
+  }
+}
+
+function evictLruAgentDatabaseHandles(): void {
+  // Callers re-fetch handles from this cache at each operation entry and use
+  // them within one synchronous section, so eviction can never close a handle
+  // mid-use; a handle retained across an eviction-triggering open goes stale.
+  while (cachedDatabases.size >= OPENCLAW_AGENT_DB_OPEN_HANDLE_CAP) {
+    let evicted = false;
+    for (const [pathname, database] of cachedDatabases) {
+      // A synchronous transaction owns its handle through COMMIT or ROLLBACK;
+      // closing it here would violate the transaction commit-section contract.
+      if (database.db.isTransaction) {
+        continue;
+      }
+      // Registry rows are durable discovery metadata; only explicit disposal
+      // unregisters them, while eviction closes this process-local handle.
+      closeCachedOpenClawAgentDatabase(database, { eviction: true });
+      cachedDatabases.delete(pathname);
+      agentDbLog.debug("evicted OpenClaw agent database handle", {
+        agentId: database.agentId,
+        openHandles: cachedDatabases.size,
+        path: pathname,
+      });
+      evicted = true;
+      break;
+    }
+    if (!evicted) {
+      // Every handle is mid-transaction: sync commit sections bound concurrent
+      // transactions at call-nesting depth, so this stays a pathological safety
+      // valve; exceeding the cap beats failing an unrelated agent's open.
+      agentDbLog.warn(
+        "agent database handle cap exceeded; all cached handles are in transactions",
+        {
+          cap: OPENCLAW_AGENT_DB_OPEN_HANDLE_CAP,
+          openHandles: cachedDatabases.size,
+        },
+      );
+      return;
+    }
   }
 }
 
@@ -352,6 +422,8 @@ export function disposeOpenClawAgentDatabaseByPath(
   // Require the cache's exact lexical owner. Following a symlink or accepting
   // an uncached path could unregister a database another process now owns.
   const resolvedPath = path.resolve(pathname);
+  // Disposal can be followed by file deletion or recreation, so revalidate next open.
+  validatedAgentDatabasePaths.delete(resolvedPath);
   const database = cachedDatabases.get(resolvedPath);
   if (!database || database.path !== resolvedPath) {
     return false;
@@ -383,5 +455,6 @@ export function closeOpenClawAgentDatabases(): void {
 /** Close cached agent handles and clear terminal failure latches for test isolation. */
 export function closeOpenClawAgentDatabasesForTest(): void {
   closeOpenClawAgentDatabases();
+  validatedAgentDatabasePaths.clear();
   terminalOpenLatch.clearAll();
 }

TheLightbulbCompany/agent

(cherry picked from commit 13ed8b5aa18cdff8996faf49901864c2aced0111)

src/state/openclaw-agent-db.ts
@@ -137,6 +137,9 @@ const OPENCLAW_AGENT_MAINTENANCE_SCHEMA_COMPATIBILITY = {
     },
   ],
 } satisfies SqliteSchemaCompatibility;
+// Each WAL database consumes roughly three file descriptors, so the fixed cap
+// satisfies the bounded-cache policy within a predictable FD budget, without config.
+export const OPENCLAW_AGENT_DB_OPEN_HANDLE_CAP = 64;
 const agentDbLog = createSubsystemLogger("state/agent-db");
 
 /** Open per-agent SQLite database handle plus lifecycle maintenance. */
@@ -165,12 +168,17 @@ type OpenClawAgentMetadataDatabase = Pick<OpenClawAgentKyselyDatabase, "schema_m
 type OpenClawAgentRegistryDatabase = Pick<OpenClawStateKyselyDatabase, "agent_databases">;
 
 const cachedDatabases = new Map<string, OpenClawAgentDatabase>();
+// External schema changes under a live process are unsupported: doctor migrations
+// require restart, so successful owner/schema validation is process-stable.
+const validatedAgentDatabasePaths = new Map<string, string>();
 const terminalOpenLatch = createSqliteTerminalOpenLatch({
   closeByPath: closeOpenClawAgentDatabaseByPath,
 });
 
 /** Latch background verification damage so later opens fail without rescanning. */
 export function recordOpenClawAgentDatabaseOpenFailure(pathname: string, error: Error): void {
+  // Quarantine revokes this process's trust because doctor may replace the file.
+  validatedAgentDatabasePaths.delete(path.resolve(pathname));
   terminalOpenLatch.record(pathname, error);
 }
 
@@ -1073,6 +1081,8 @@ export function openOpenClawAgentDatabase(
         `OpenClaw agent database ${pathname} is already open for agent ${cached.agentId}; requested agent ${agentId}.`,
       );
     }
+    cachedDatabases.delete(pathname);
+    cachedDatabases.set(pathname, cached);
     return cached;
   }
   // Latched paths are quarantined; every fresh open fails fast here until
@@ -1107,14 +1117,24 @@ export function openOpenClawAgentDatabase(
   }
   const openStartedAt = Date.now();
   ensureOpenClawAgentDatabasePermissions(pathname, databaseOptions);
+  // Free a slot before constructing the new handle: under real descriptor
+  // pressure the 65th open would otherwise fail before eviction could run.
+  evictLruAgentDatabaseHandles();
   const sqlite = requireNodeSqlite();
   const db = new sqlite.DatabaseSync(pathname);
+  // Eviction churn must avoid schema/registry busy waits on the event loop while
+  // reconcile workers hold write transactions on these same agent databases.
+  const isValidatedReopen = validatedAgentDatabasePaths.get(pathname) === agentId;
   const walMaintenance = (() => {
     let maintenance: SqliteWalMaintenance | undefined;
     try {
       db.exec(`PRAGMA busy_timeout = ${OPENCLAW_SQLITE_BUSY_TIMEOUT_MS};`);
-      assertSupportedAgentSchemaVersion(db, pathname);
-      assertExistingSchemaOwner(readExistingSchemaMeta(db), agentId, pathname);
+      if (!isValidatedReopen) {
+        assertSupportedAgentSchemaVersion(db, pathname);
+        assertExistingSchemaOwner(readExistingSchemaMeta(db), agentId, pathname);
+      }
+      // Integrity is not process-stable: the file can be damaged while evicted.
+      // This guard is read-only (no busy waits), so every physical open pays it.
       assertAgentDatabaseIntegrityBeforeMutation(db, pathname);
       configureSqlitePreSchemaPragmas(db, {
         busyTimeoutMs: OPENCLAW_SQLITE_BUSY_TIMEOUT_MS,
@@ -1126,7 +1146,9 @@ export function openOpenClawAgentDatabase(
         foreignKeys: true,
         synchronous: "NORMAL",
       });
-      ensureAgentSchema(db, agentId, pathname);
+      if (!isValidatedReopen) {
+        ensureAgentSchema(db, agentId, pathname);
+      }
       return maintenance;
     } catch (err) {
       maintenance?.close();
@@ -1142,11 +1164,14 @@ export function openOpenClawAgentDatabase(
   })();
   ensureOpenClawAgentDatabasePermissions(pathname, databaseOptions);
   const database = { agentId, db, path: pathname, walMaintenance };
-  try {
-    registerAgentDatabase({ agentId, path: pathname, env: options.env });
-  } catch (error) {
-    closeCachedOpenClawAgentDatabase(database);
-    throw error;
+  if (!isValidatedReopen) {
+    try {
+      registerAgentDatabase({ agentId, path: pathname, env: options.env });
+    } catch (error) {
+      closeCachedOpenClawAgentDatabase(database);
+      throw error;
+    }
+    validatedAgentDatabasePaths.set(pathname, agentId);
   }
   cachedDatabases.set(pathname, database);
   terminalOpenLatch.clear(pathname);
@@ -1232,11 +1257,56 @@ export function runOpenClawAgentWriteTransaction<T>(
 
 let unregisterExitClose: (() => void) | null = null;
 
-function closeCachedOpenClawAgentDatabase(database: OpenClawAgentDatabase): void {
-  database.walMaintenance.close();
+function closeCachedOpenClawAgentDatabase(
+  database: OpenClawAgentDatabase,
+  options: { eviction?: boolean } = {},
+): void {
+  // Eviction must stay cheap: PASSIVE skips waiting on concurrent readers,
+  // whose drained TRUNCATE checkpoints blocked the event loop for seconds.
+  database.walMaintenance.close(options.eviction ? { checkpointMode: "PASSIVE" } : undefined);
   clearNodeSqliteKyselyCacheForDatabase(database.db);
   if (database.db.isOpen) {
     database.db.close();
+  }
+}
+
+function evictLruAgentDatabaseHandles(): void {
+  // Callers re-fetch handles from this cache at each operation entry and use
+  // them within one synchronous section, so eviction can never close a handle
+  // mid-use; a handle retained across an eviction-triggering open goes stale.
+  while (cachedDatabases.size >= OPENCLAW_AGENT_DB_OPEN_HANDLE_CAP) {
+    let evicted = false;
+    for (const [pathname, database] of cachedDatabases) {
+      // A synchronous transaction owns its handle through COMMIT or ROLLBACK;
+      // closing it here would violate the transaction commit-section contract.
+      if (database.db.isTransaction) {
+        continue;
+      }
+      // Registry rows are durable discovery metadata; only explicit disposal
+      // unregisters them, while eviction closes this process-local handle.
+      closeCachedOpenClawAgentDatabase(database, { eviction: true });
+      cachedDatabases.delete(pathname);
+      agentDbLog.debug("evicted OpenClaw agent database handle", {
+        agentId: database.agentId,
+        openHandles: cachedDatabases.size,
+        path: pathname,
+      });
+      evicted = true;
+      break;
+    }
+    if (!evicted) {
+      // Every handle is mid-transaction: sync commit sections bound concurrent
+      // transactions at call-nesting depth, so this stays a pathological safety
+      // valve; exceeding the cap beats failing an unrelated agent's open.
+      agentDbLog.warn(
+        "agent database handle cap exceeded; all cached handles are in transactions",
+        {
+          cap: OPENCLAW_AGENT_DB_OPEN_HANDLE_CAP,
+          openHandles: cachedDatabases.size,
+        },
+      );
+      return;
+    }
   }
 }
 
@@ -1272,6 +1342,8 @@ export function disposeOpenClawAgentDatabaseByPath(
   // Require the cache's exact lexical owner. Following a symlink or accepting
   // an uncached path could unregister a database another process now owns.
   const resolvedPath = path.resolve(pathname);
+  // Disposal can be followed by file deletion or recreation, so revalidate next open.
+  validatedAgentDatabasePaths.delete(resolvedPath);
   const database = cachedDatabases.get(resolvedPath);
   if (!database || database.path !== resolvedPath) {
     return false;
@@ -1303,6 +1375,7 @@ export function closeOpenClawAgentDatabases(): void {
 /** Close cached agent handles and clear terminal failure latches for test isolation. */
 export function closeOpenClawAgentDatabasesForTest(): void {
   closeOpenClawAgentDatabases();
+  validatedAgentDatabasePaths.clear();
   terminalOpenLatch.clearAll();
 }
 /* oxlint-disable max-lines -- TODO: split this grandfathered oversized file. */